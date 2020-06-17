e77d72469dd327699425806ec5ec9886fdd6534f
Nathaniel Aaron Bond <nabond251@gmail.com>
2020-06-17 05:05:14 -0400

    Add project files.

---
+++ SdgApps.TimeWise.ActivityJournal/SdgApps.TimeWise.ActivityJournal/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using SdgApps.TimeWise.ActivityJournal.Models;
+
+namespace SdgApps.TimeWise.ActivityJournal.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
